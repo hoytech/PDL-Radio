5ad6bd432c6277431e1001fe69cd976cb49c1dee
doug <doug@hcsw.org>
2013-12-05 06:13:37 -0500

    update synopsis

--- lib/PDL/Radio.pm
+++ lib/PDL/Radio.pm
@@ -42,28 +42,63 @@ PDL::Radio - Amateur radio system built on PDL
 
 =head1 SYNOPSIS
 
-    ## Play 1000 Hz sine wave for 5 seconds:
-    PDL::Radio->new->play("sine", 5, 1000);
+    use PDL::Radio::All;
 
-    ## Play .5 seconds of a sine wave, phase shift 180 degrees, play another .5:
-    PDL::Radio->new->play("sine", .5, 1000)
-                   ->play("sine", .5, 1000, PI);
 
-    ## Plot 5 periods of a sawtooth wave:
-    PDL::Radio->new->plot("sawtooth", 5, 1);
+    ## Basic sounds for testing:
 
-    ## Play PSK-31 encoded message
-    PDL::Radio->new->play("psk", 1000, "hello world!");
+    ## Play 500 Hz sine wave for 5 seconds:
+    PDL::Radio::Sine->new(freq => 500)->play(5);
+
+    ## Play 1 second of a sine wave, phase shift 180 degrees, play another second:
+    PDL::Radio::Sine->new->play(1)->play(1,PI);
+
+    ## Plot .01 second of a sawtooth wave:
+    plot(PDL::Radio::Sawtooth->new->get(.01));
+
+
+    ## CW (morse code):
+
+    ## Call CQ:
+    PDL::Radio::CW->new->play("CQ CQ CQ DE VE3HOY VE3HOY " x 10);
+
+    ## Change keying envelope (hard/min/hanning):
+    PDL::Radio::CW->new(shape => 'hard')->play("CQ CQ CQ");
+
+    ## Risetime in milliseconds:
+    PDL::Radio::CW->new(shape => 'hanning', risetime => 10)->play("CQ CQ CQ");
+
+
+    ## RTTY:
+
+    ## Call CQ:
+    PDL::Radio::RTTY->new->play("CQ CQ CQ DE VE3HOY VE3HOY " x 10);
+
+    ## Custom baud/freq_shift:
+    PDL::Radio::RTTY->new(freq_shift => 90, baud => 20)->play("CQ CQ CQ");
+
+
+    ## PSK-31:
+
+    ## Call CQ:
+    PDL::Radio::PSK->new->play("cq cq cq de VE3HOY VE3HOY " x 10);
+
+    ## Transmit all 0s (two tones):
+    PDL::Radio::PSK->new->play("\x00"x100, 800);
+
+    ## Transmit all 1s (single tone):
+    PDL::Radio::PSK->new->play("\xFF"x100, 800);
+    
 
 =head1 DESCRIPTION
 
-This is a work-in-progress library for generating sound data and playing it through L<Pulse Audio|http://www.freedesktop.org/wiki/Software/PulseAudio/>. Pulse Audio makes redirecting the audio output to programs like L<fldigi|http://www.w1hkj.com/Fldigi.html> really easy.
+This is a work-in-progress library for generating modem data and playing it through L<Pulse Audio|http://www.freedesktop.org/wiki/Software/PulseAudio/>. Pulse Audio makes redirecting the audio output to programs like L<fldigi|http://www.w1hkj.com/Fldigi.html> really easy.
 
 =head1 SEE ALSO
 
 L<The PDL::Radio github repo|https://github.com/hoytech/PDL-Radio>
 
-L<PDL::Audio> is very similar to this module except that it interfaces to sndlib where this module interfaces to pulseaudio. L<PDL::Audio> is a pain to setup and I couldn't figure out how to send data to fldigi. Also, L<Pulse::Audio> doesn't seem to be on CPAN anymore?
+L<PDL::Audio> is similar to this module except that it interfaces to sndlib where this module interfaces to pulseaudio. L<PDL::Audio> is a pain to setup and I couldn't figure out how to send data to fldigi. Also, L<Pulse::Audio> doesn't seem to be on CPAN anymore?
 
 =head1 AUTHOR
 
